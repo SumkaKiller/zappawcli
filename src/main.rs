15cede47be562fe33ad1d2b77b71f8847ea67c2f
jokerdayn <sasab1017@gmail.com>
2024-04-14 17:13:20 +0000

    chore(build): register tree modules and primary dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
-}
+mod app;
+mod commands;
+mod input;
+mod text;
+mod ui;
+
+use crossterm::{ cursor, event::{self, Event, KeyCode, KeyEventKind, KeyModifiers}, execute, style::ResetColor, terminal };
+use std::io;
+use std::time::{Duration, Instant};
+use app::{App, HelpState};
+use commands::APP_NAME;
+use ui::render::{prompt_nickname, render};
+
+fn main() {}
